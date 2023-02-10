1ef2ca64bccca4add369724865a78dfbe6c18964
André Grillo <andregrillo@gmail.com>
2023-02-10 16:06:57 +0000

    Android implemented

--- src/android/MyNotifications.java
+++ src/android/MyNotifications.java
@@ -1,32 +1,68 @@
-package outsystems-plugin-mynotifications;
+package outsystems.plugin.mynotifications;
 
-import org.apache.cordova.CordovaPlugin;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+import android.os.Handler;
+
+import androidx.annotation.RequiresApi;
+
 import org.apache.cordova.CallbackContext;
-
+import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
+
+import io.cordova.hellocordova.R;
+
 
 /**
  * This class echoes a string called from JavaScript.
  */
 public class MyNotifications extends CordovaPlugin {
 
+    private final String ACTION_SEND_NOTIFICATION = "sendNotification";
+
+    private NotificationManager notificationManager;
+
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
+        if (action.equals(ACTION_SEND_NOTIFICATION)) {
+
+            String title = (String) args.get(0);
+            String message = (String) args.get(1);
+            Integer seconds = (Integer) args.get(2);
+
+            try {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    notificationManager = (NotificationManager) this.cordova.getActivity().getSystemService(Context.NOTIFICATION_SERVICE);
+                    createNotificationChannel(title, message, seconds, callbackContext);
+                }
+            } catch (Exception ex) {
+                callbackContext.error(ex.getMessage());
+            }
             return true;
         }
         return false;
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
-        }
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    protected void createNotificationChannel(String title, String message, Integer seconds, CallbackContext callbackContext) {
+        int notificationID = 101;
+        String channelID = cordova.getActivity().getPackageName();
+
+        Notification notification =
+                new Notification.Builder(this.cordova.getContext(), channelID)
+                        .setContentTitle(title)
+                        .setContentText(message)
+                        .setSmallIcon(R.drawable.ic_cdv_splashscreen)
+                        .setChannelId(channelID)
+                        .build();
+
+        final Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            notificationManager.notify(notificationID, notification);
+            callbackContext.success();
+        }, seconds * 1000);
     }
 }
